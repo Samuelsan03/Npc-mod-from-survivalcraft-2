d4b385ed7ab179d06a0f78467ebac99459c59b8e
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-11-06 10:40:25 -0500

    Add files via upload

---
+++ Criaturas De Mierda/Criaturas De Mierda/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criaturas_De_Mierda
+{
+    public class Class1
+    {
+    }
+}
